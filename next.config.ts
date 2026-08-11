96017d48cad89e24786c5019100fd66152a41245
carlosBenitez0 <cb2724136@gmail.com>
2026-08-11 12:47:46 -0600

    fix: use pg driver adapter for prisma client

--- next.config.ts
+++ next.config.ts
@@ -13,7 +13,7 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  serverExternalPackages: ['@prisma/client', 'bcryptjs'],
+  serverExternalPackages: ['@prisma/client', 'bcryptjs', 'pg'],
 };
 
 export default nextConfig;
